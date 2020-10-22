b0639fb6a11bbcf721a0315c3715f758c0118dd5
USSI6360 <kudan.mugen@gmail.com>
2020-10-23 08:55:36 +0900

    usersテーブルの作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,11 @@
 class ApplicationController < ActionController::Base
    before_action :basic_auth
-
-
+   before_action :configure_permitted_parameter, if: :devise_controller?
 
    private
+   def configuer_permitted_parameters
+       devise_parameter_sanitizer.permit(:sign_up,key: [:nickname])
+   end
 
    def basic_auth
       authenticate_or_request_with_http_basic do |username, password|
--- app/controllers/furima_controller.rb
+++ app/controllers/furima_controller.rb
@@ -1,6 +1,14 @@
 class FurimaController < ApplicationController
+  before_action :move_to_index, except: [:index, :show]
 
   def index
     @furima = Furima.all
+  end
+
+  private
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 end
