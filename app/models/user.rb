938d0de312ce12c3419af8328afa058bc91e6006
USSI6360 <kudan.mugen@gmail.com>
2020-10-26 14:08:25 +0900

    修正２

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,11 @@ class User < ApplicationRecord
 
          with_options presence: true do
          validates :nickname, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
+         validates :password, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
          validates :birth_date, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
          validates :family_name, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
          validates :name, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
          validates :family_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
-         validates :name_kana   presence: true format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
+         validates :name_kana,   format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
          end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,12 @@
 FactoryBot.define do
   factory :user do
-    nickname              {Faker: :Name.initials(number: 2)}
-    email                 {Faker: :Internet. free_email}
-    password              {Faker: :Internet.password(min_length: 6)}
+    nickname              {Faker :Name.initials(number: 2)}
+    email                 {Faker :Internet. free_email}
+    password              {Faker :Internet.password(min_length 6)}
     password_confirmation {password}
-    birth_date            {Faker: :Internet. free_birth_date}
-    family_name           {Faker: :Name.initials(number: 2)}
-    name                  {Faker: :Name.initials(number: 2)}
-    family_name_kana      {Faker: :Name.initials(number: 2)}
-    name_kana             {Faker: :Name.initials(number: 2)}
+    family_name           {Faker :Name.initials(number: 2)}
+    name                  {Faker :Name.initials(number: 2)}
+    family_name_kana      {Faker :Name.initials(number: 2)}
+    name_kana             {Faker :Name.initials(number: 2)}
   end
 end
