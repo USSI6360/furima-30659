70cf6bdf90fa4825f04c04debf2c8eb20fde4219
USSI6360 <kudan.mugen@gmail.com>
2020-11-03 15:58:09 +0900

    render完了

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -31,6 +31,11 @@ class ItemsController < ApplicationController
   def update
     item = Item.find(params[:id])
     item.update(item_params)
+    if current_user.update(user_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
   end
 
   private
