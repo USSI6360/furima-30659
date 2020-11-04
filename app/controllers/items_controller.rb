162adba1240e26b4eda074dd9a16e0c3bd274eb5
USSI6360 <kudan.mugen@gmail.com>
2020-11-04 10:30:53 +0900

    修正a

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -35,7 +35,10 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    @item.destroy(item_params)
+    if current_user.id == @item.user.id
+      @item.destroy
+      redirect_to root_path
+      end
   end
 
 
