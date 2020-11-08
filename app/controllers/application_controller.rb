b3378a00a839ce3f8d647946a58859b5ddb05960
USSI6360 <kudan.mugen@gmail.com>
2020-11-08 22:50:06 +0900

    basic認証

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
 
    def basic_auth
       authenticate_or_request_with_http_basic do |username, password|
-        username == '' && password ==''
+        username == 'Ussii' && password =='XXX-01H'
       end
     end
 end
