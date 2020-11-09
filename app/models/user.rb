aa5fbd4a15e1383f51346027511476cd757fc354
USSI6360 <kudan.mugen@gmail.com>
2020-11-09 17:21:34 +0900

    バリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ApplicationRecord
          
          with_options presence: true do
          validates :nickname
+         validates :password, format: { with: /\A[a-zA-Z0-9]+\z/}
          validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥]/}
          validates :name, format: { with: /\A[ぁ-んァ-ン一-龥]/}
          validates :family_name_kana, format: { with: /\A[ァ-ヶー－]+\z/}
