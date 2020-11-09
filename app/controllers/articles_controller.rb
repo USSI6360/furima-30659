df28212e78c5df0251090a6453441b1c2c01dbbd
USSI6360 <kudan.mugen@gmail.com>
2020-11-09 12:49:52 +0900

    /articles_controller.rb　消去

--- app/controllers/articles_controller.rb
+++
@@ -1,7 +0,0 @@
-  # def new
-  #   @burden = Burden.order("created_at DESC")
-  #   @category = Category.order("created_at DESC")
-  #   @day = Day.order("created_at DESC")
-  #   @prefecture = Prefecture.order("created_at DESC")
-  #   @status = Status.order("created_at DESC")
-  # end
